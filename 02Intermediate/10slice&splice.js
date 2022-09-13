3ce6f1e581c9de5248f8a67071937b0a38190a7f
hdubey64 <hdubey64@gmail.com>
2022-09-13 17:33:55 +0530

    Slice and Splice in JavaScript

--- 02Intermediate/10slice&splice.js
+++ 02Intermediate/10slice&splice.js
@@ -1,1 +1,19 @@
 var user = ["Vishal", "Shashank", "Ratnesh", "Abhishek", "Ajeet", "Ajay"];
+
+// console.log(user.slice(1, 3));
+// return:- [ 'Shashank', 'Ratnesh' ]
+
+// console.log(user.slice(1));
+// return:- [ 'Shashank', 'Ratnesh', 'Abhishek', 'Ajeet', 'Ajay' ]
+
+// user.splice(1, 2, "HI");
+// console.log(user);
+// return:- [ 'Vishal', 'HI', 'Abhishek', 'Ajeet', 'Ajay' ]
+
+// user.splice(1, 3, "HI");
+// console.log(user);
+// return;- [ 'Vishal', 'HI', 'Ajeet', 'Ajay' ]
+
+user.splice(1, 3, "HI", "BYE");
+console.log(user);
+//return:- [ 'Vishal', 'HI', 'BYE', 'Ajeet', 'Ajay' ]
