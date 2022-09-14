06190e532b7207160e53e0177e8a6d45f8596e53
hdubey64 <hdubey64@gmail.com>
2022-09-14 16:47:55 +0530

    While And Do While Loop In JavaScript

---
+++ 02Intermediate/14while&DoWhileLoop.js
@@ -0,0 +1,46 @@
+const myStates = [
+   "Utter Pradesh",
+   "Delhi",
+   "Assam",
+   1947,
+   "Tamil Nadu",
+   "Maharastra",
+];
+
+// let i = 0;
+// while (i < myStates.length) {
+//    console.log(myStates[i]);
+//    i++;
+// }
+
+/* return:- Utter Pradesh
+            Delhi
+            Assam
+            1947
+            Tamil Nadu
+            Maharastra
+*/
+
+// let i = 0;
+// do {
+//    console.log(i);
+//    i++;
+// } while (i < 10);
+/* return:- 0
+            1
+            2
+            3
+            4
+            5
+            6
+            7
+            8
+            9
+*/
+
+let i = 20;
+do {
+   console.log(i);
+   i++;
+} while (i < 10);
+/* return:- 20*/
