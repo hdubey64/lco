129d35d6513eb0afb3da9e6edf3527505ee898a7
hdubey64 <hdubey64@gmail.com>
2022-09-14 11:45:28 +0530

    Methode and Object Complete

--- 02Intermediate/12objectPartTwo.js
+++ 02Intermediate/12objectPartTwo.js
@@ -11,9 +11,20 @@ var user = {
    getCourseCount: function () {
       return `${this.firstName} is enrilled in total of ${this.courseList.length}`;
    },
+   info: function () {
+      console.log(
+         `${this.firstName} ${this.lastName} role of ${
+            this.role
+         }, login count is ${this.logInCount}, loggedin by ${
+            this.facebookSigned ? "Facebook" : "Geust"
+         } and his course list is`
+      );
+      console.table(this.courseList);
+   },
 };
 
 var courseList = true;
+
 console.log(user.firstName);
 //return:- Vishal
 
@@ -29,5 +40,7 @@ console.log(user.getCourseCount());
 //return:- Vishal is enrilled in total of 2
 
 console.table(user);
+
+user.info();
 
 // TODO: Creat a method info:- From first name to how much courseList
