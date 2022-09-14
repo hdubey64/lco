ed8d1bcad0b0145dec5f63df6f1df38b291112c8
hdubey64 <hdubey64@gmail.com>
2022-09-14 18:27:35 +0530

    For Each Loop In Java Script

---
+++ 02Intermediate/15forEachLoop.js
@@ -0,0 +1,23 @@
+const myStates = [
+   "Utter Pradesh",
+   "Delhi",
+   "Assam",
+   1947,
+   "Tamil Nadu",
+   "Maharastra",
+];
+// var i = 0;
+// for (;;) {
+//    if (i > 3) break;
+//    console.log(i);
+//    i++;
+// }
+
+myStates.forEach((s) => console.log(s));
+/* return:- Utter Pradesh
+            Delhi
+            Assam
+            1947
+            Tamil Nadu
+            Maharastra
+*/
