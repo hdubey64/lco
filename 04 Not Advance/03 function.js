ea5db4ba8841303127182684c283dea432b8399c
hdubey64 <hdubey64@gmail.com>
2022-09-19 16:01:09 +0530

    Self Exceuting Anonymous Function And Function Programiing

---
+++ 04 Not Advance/03 function.js
@@ -0,0 +1,17 @@
+(function () {
+   console.log("I say Hello");
+   console.log("I Again say Hello");
+})();
+
+// let score = 60;
+
+// function addBonus() {
+//    score = score + 45;
+//    return score;
+// }
+
+// console.log(addBonus());
+// functionadddBonus(score),{
+//    return score + 30;
+// }
+// console.log(functionadddBonus());
