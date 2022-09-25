5fe875ebba466e2be415352d5f7a229cce2ed127
hdubey64 <hdubey64@gmail.com>
2022-09-25 11:09:31 +0530

    Template Literals in JavaScript

---
+++ 05 Adavnceish/02 String.js
@@ -0,0 +1,20 @@
+var name = `Vishal`;
+var fullName = `${name} Dubey`;
+
+console.log(fullName);
+//return:- Vishal Dubey
+
+console.log(fullName.charAt(0));
+//return:- V
+
+console.log(fullName.endsWith("y"));
+//return:- true
+
+console.log(fullName.includes("Vishal"));
+//return :- true
+
+console.log(fullName.toLocaleLowerCase());
+// return:- vishal dubey
+
+console.log(fullName.toLocaleUpperCase());
+// return:- VISHAL DUBEY
