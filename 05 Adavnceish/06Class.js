220dd05ee1818f5af3314cd328b556c4e887efc1
hdubey64 <hdubey64@gmail.com>
2022-09-26 11:15:20 +0530

    Private Props Getters and Sentters In JavaScript

--- 05 Adavnceish/06Class.js
+++ 05 Adavnceish/06Class.js
@@ -3,19 +3,25 @@ class User {
       this.name = name;
       this.email = email;
    }
-   courseList = [];
+   #courseList = [];
 
    getInfo() {
       return { name: this.name, email: this.email };
    }
    enrollCourse(name) {
-      this.courseList.push(name);
+      this.#courseList.push(name);
    }
    getCousreList() {
-      return this.courseList;
+      return this.#courseList;
    }
 }
 
 let vishal = new User("vishal", "hdubey64@gmail.com");
 
 module.exports = User;
+
+const rock = new User("rock", "rock@rock.com");
+console.log(rock.getInfo());
+rock.enrollCourse("Angular Bootcamp");
+console.log(rock.getCousreList());
+console.log(rock.courseList);
